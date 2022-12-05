8cd6ae4860a0ef7a1f17529ddf0590361ee7fea2
brickwall <sk292207@gmail.com>
2022-12-05 10:54:09 +0530

    Make bishop move without jumping over any of pieces

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -5,13 +5,14 @@ class Bishop < Abstract_Piece
 
     UNIT_LENGTH = 1
 
-    attr_reader :color, :piece_id, :rendered, :relative_moves
+    attr_reader :color, :piece_id, :rendered, :relative_moves, :board
 
-    def initialize(color, piece_id)
+    def initialize(color, piece_id, board)
         @color = color
         @piece_id = piece_id
         @rendered = piece(color)
         @relative_moves = []
+        @board = board
     end
 
     def piece(color)
@@ -27,8 +28,16 @@ class Bishop < Abstract_Piece
             diagonal_length = [(current_file.ord - bound[:file].ord).abs, (current_rank - bound[:rank]).abs].min
             UNIT_LENGTH.upto(diagonal_length) do |multiplier|
                 @relative_moves << unit_relative_move.map{|i| i*multiplier }
+                unless board.square_available?(forge_coordinate(relative_moves.last, current_file, current_rank)) 
+                    @relative_moves.pop
+                    break
+                end
             end
         end
+    end
+
+    def forge_coordinate(relative_move, current_file, current_rank)
+        [(relative_move[0] + current_file.ord).chr, relative_move[1] + current_rank].join
     end
 
     def potential_squares(slashed_coordinate)
@@ -36,7 +45,7 @@ class Bishop < Abstract_Piece
         current_rank = slashed_coordinate[1].to_i
         diagonals_available(current_file, current_rank)
         relative_moves.map do |relative_move|
-            [(relative_move[0]+current_file.ord).chr,relative_move[1]+current_rank].join
+            forge_coordinate(relative_move, current_file, current_rank)
         end
     end
 end
--- lib/setup.rb
+++ lib/setup.rb
@@ -14,7 +14,7 @@ class Setup
     def init_knights
         knights = []
         {'b1'=>:white,'g1'=>:white,'g8'=>:black,'b8'=>:black}.each_with_index do |(coordinate, color), index|
-            knights << Knight.new(color,:N)
+            knights << Knight.new(color,:N, board)
             board.grid[coordinate][:piece] = knights[index]
             board.grid[coordinate][:square][11] = knights[index].rendered
             #the property of pieces should not be the property of grid but of piece itself 
@@ -24,7 +24,7 @@ class Setup
     def init_bishops
         bishops = []
         {'c1'=>:white,'f1'=>:white,'c8'=>:black,'f8'=>:black}.each_with_index do |(coordinate, color), index|
-            bishops << Bishop.new(color, :B)
+            bishops << Bishop.new(color, :B, board)
             board.grid[coordinate][:piece] = bishops[index]
             board.grid[coordinate][:square][11] = bishops[index].rendered
         end
@@ -33,7 +33,7 @@ class Setup
     def init_rooks
         rooks = []
         {'a1'=>:white,'h1'=>:white,'a8'=>:black,'h8'=>:black}.each_with_index do |(coordinate, color), index|
-            rooks << Rook.new(color, :R)
+            rooks << Rook.new(color, :R, board)
             board.grid[coordinate][:piece] = rooks[index]
             board.grid[coordinate][:square][11] = rooks[index].rendered
         end
