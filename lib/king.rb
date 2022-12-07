63f2fb5bee18d9016337136b7a1514139871e66c
brickwall <sk292207@gmail.com>
2022-12-07 16:28:03 +0530

    Add feature to king so that it can capture pieces

--- lib/king.rb
+++ lib/king.rb
@@ -21,12 +21,20 @@ class King < Abstract_Piece
   def potential_squares(slashed_coordinate)
     current_file = slashed_coordinate[0]
     current_rank = slashed_coordinate[1].to_i
-    relative_moves.filter do |relative_move|
+    all_squares = relative_moves.filter do |relative_move|
       (relative_move[0] + current_file.ord).chr.between?('a', 'h') && (relative_move[1] + current_rank).between?(1, 8)
     end.map do |relative_move|
       forge_coordinate(relative_move, current_file, current_rank)
-    end.filter \
+    end
+    yield(all_squares) if block_given?
+    possible_squares = all_squares.filter \
     { |coordinate| board.square_available?(coordinate) }
+  end
+
+  def capture_possible?(current_coordinate, destination)
+    a = potential_squares(current_coordinate) do |all_squares|
+      return all_squares.include?(destination) 
+    end
   end
 
 end
