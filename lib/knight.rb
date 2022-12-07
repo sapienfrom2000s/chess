5fe4c78ff3b18ed6032a7aeb9e6afcb47ebb3ac3
brickwall <sk292207@gmail.com>
2022-12-07 12:44:01 +0530

    Add feature to knight so that it can make valid captures

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,4 +1,5 @@
 require_relative 'abstract_piece'
+require 'pry-byebug'
 
 class Knight < Abstract_Piece
   attr_reader :color, :piece_id, :rendered, :relative_moves, :board
@@ -19,15 +20,22 @@ class Knight < Abstract_Piece
     @relative_moves = [[-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1]]
   end
 
-  def potential_squares(slashed_coordinate)
-    current_file = slashed_coordinate[0]
-    current_rank = slashed_coordinate[1].to_i
-    relative_moves.filter do |relative_move|
+  def potential_squares(coordinate)
+    current_file = coordinate[0]
+    current_rank = coordinate[1].to_i
+    all_squares = relative_moves.filter do |relative_move|
       (relative_move[0] + current_file.ord).chr.between?('a', 'h') && (relative_move[1] + current_rank).between?(1, 8)
     end.map do |relative_move|
       forge_coordinate(relative_move, current_file, current_rank)
-    end.filter \
+    end
+    yield(all_squares) if block_given?
+    valid_squares = all_squares.filter \
     { |coordinate| board.square_available?(coordinate) }
   end
 
+  def capture_possible?(current_coordinate, destination)
+   a = potential_squares(current_coordinate) do |all_squares|
+     return all_squares.include?(destination) 
+   end
+  end
 end
